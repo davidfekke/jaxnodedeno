3824b563f7e98ce58fea45d71306b9ed52a86e8f
David Fekke <david@fekke.com>
2020-07-07 12:08:42 -0400

    Fixing problem with variable being wrong type

--- dogyearserver.ts
+++ dogyearserver.ts
@@ -1,15 +1,26 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-import dogYears from './dogYears.ts'
+import dogYears from './dogyears.ts'
+import dogYears2 from './dogyears.js'
 
 const app = new Application();
 
 const router = new Router();
 
+router.get('/', (ctx) => {
+    const dAge = 12;
+    const dWeight = 50;
+    const humanage = dogYears(dAge, dWeight);
+    const result = { "humanage": `${humanage}` };
+    ctx.response.body = result; 
+});
+
 router.get('/age/:age/weight/:weight', (ctx) => {
     if (ctx.params && ctx.params.age && ctx.params.weight) {
-        const dAge: number = ctx.params.age as any as number;
-        const dWeight = ctx.params.weight as any as number;
-        ctx.response.body = dogYears(dAge, dWeight);
+        const dAge: number = parseFloat(ctx.params.age) as number;
+        const dWeight: number = parseFloat(ctx.params.weight) as number;
+        const humanage = dogYears(dAge, dWeight);
+        const result = { "humanage": `${humanage}` };
+        ctx.response.body = result;
     }
 });
 
