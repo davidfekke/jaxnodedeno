13a5bc745f6750928a29e85cedb1f38199d5e495
David Fekke <david@fekke.com>
2020-07-08 09:14:31 -0400

    Adding sky cover format and array checking

--- getcraigweather.js
+++ getcraigweather.js
@@ -8,13 +8,30 @@ const json = await result.json();
 json.reports.forEach(report => {
     //console.log(report);
     console.log(`observation_time: ${report.observation_time}`);
-    console.log(`temp_c: ${report.temp_c}`);
-    console.log(`dewpoint: ${report.dewpoint_c}`);
+    console.log(`temp: ${report.temp_c} (Celcius)`);
+    console.log(`dewpoint: ${report.dewpoint_c} (Celcius)`);
     console.log(`wind: ${report.wind_dir_degrees} at ${report.wind_speed_kt} knots`);
     console.log(`visibility: ${report.visibility_statute_mi}`);
-    console.log(`altimeter: ${report.altim_in_hg}`);
-    console.log(report.flight_category);
-    report.sky_condition.forEach(sky => {
-        console.log(`${sky.sky_cover} at ${sky.cloud_base_ft_agl} feet.`);
-    });
+    console.log(`altimeter: ${report.altim_in_hg.toFixed(2)}`);
+    console.log(`flight category: ${report.flight_category}`);
+    if (report.sky_condition) { //  && report.sky_condition.length > 0
+        if (Array.isArray(report.sky_condition)) {
+            report.sky_condition.forEach(sky => {
+            
+                if ('cloud_base_ft_agl' in sky) {
+                    console.log(`sky cover: ${sky.sky_cover} at ${sky.cloud_base_ft_agl} feet.`);
+                } else {
+                    console.log(`sky cover: ${sky.sky_cover}`);
+                }
+                
+            });
+        } else {
+            if ('cloud_base_ft_agl' in report.sky_condition) {
+                console.log(`sky cover: ${report.sky_condition.sky_cover} at ${report.sky_condition.cloud_base_ft_agl} feet.`);
+            } else {
+                console.log(`sky cover: ${report.sky_condition.sky_cover}`);
+            }
+        }
+        
+    }
 });
